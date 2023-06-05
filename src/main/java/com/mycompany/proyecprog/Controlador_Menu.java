026e674969a8f0b46d5362f0799bfce6e26c2634
FonoNow <francofonolla@gmail.com>
2023-06-05 08:46:58 -0300

    Revert "Update Controlador_Menu.java"
    This reverts commit 7137451d48fa40c384883e2b3b04b30a901252bd.

--- src/main/java/com/mycompany/proyecprog/Controlador_Menu.java
+++ src/main/java/com/mycompany/proyecprog/Controlador_Menu.java
@@ -1,8 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.proyecprog;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ *
+ * @author PC
+ */
 public class Controlador_Menu implements ActionListener{
    public Menu menu;
    public Controlador_Nuevo contnuevo;
