52056c582cbf330780205f0a935fa68af83a4d08
Gonzalo Herrera <gonzaaherre@gmail.com>
2023-06-07 13:33:19 -0300

    insertar cursos y empleado a la tabla de cursos_realizados con nfecha

--- src/main/java/datos/CursoDAO.java
+++ src/main/java/datos/CursoDAO.java
@@ -27,11 +27,11 @@ public class CursoDAO {
              "DELETE FROM cursos WHERE codigo = ?;";
      
      private static final String SQL_INSERTAR_CURSOS_REALIZADO=
-             "insert into cursos_realizados (cod_curso, id_empl) values (?,?);";
+             "insert into cursos_realizados (cod_curso, id_empl,fecha_inicio) values (?,?,?);";
      
      
      
-     public int insertar_cur_reali(unCurso curso, Empleado empleado){
+     public int insertar_cur_reali(unCurso curso, Empleado empleado, String fecha){
         Connection conn=null;
         PreparedStatement stmt= null;
         int registros = 0;
@@ -40,6 +40,7 @@ public class CursoDAO {
             stmt=conn.prepareCall(SQL_INSERTAR_CURSOS_REALIZADO);
             stmt.setInt(1, curso.getIDCodigo());
             stmt.setInt(2, empleado.getIdEmpleado());
+            stmt.setString(3, fecha);
             registros=stmt.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(EmpleadoDAO.class.getName()).log(Level.SEVERE, null, ex);
